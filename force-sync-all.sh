fe1819c65ba2d5df033d76d4cb2b0e7ccde74f2c
Jens Reidel <adrian@travitia.xyz>
2022-12-22 00:18:25 +0100

    scripts: Add script to force sync all repositories
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ force-sync-all.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+THREAD_COUNT=$(nproc --all)
+repo sync -c --jobs-network=$(( $THREAD_COUNT < 16 ? $THREAD_COUNT : 16 )) -j$THREAD_COUNT --jobs-checkout=$THREAD_COUNT --force-sync --no-clone-bundle --no-tags
