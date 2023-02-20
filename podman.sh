f04b94315a1e89c00d132d7b8630733ea3bdeca2
Jens Reidel <adrian@travitia.xyz>
2023-02-20 01:02:33 +0100

    scripts: Remove pids limit in podman
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- podman.sh
+++ podman.sh
@@ -28,25 +28,26 @@ then
 fi
 
 podman run \
-	--rm \
-	-it \
-	--privileged \
-	--ulimit=host \
-	--ipc=host \
-	--cgroups=disabled \
-	--name kenvyra \
-	--security-opt label=disable \
-	--userns=keep-id \
-	--hostname $(hostname) \
-	--mount type=tmpfs,tmpfs-size=1G,destination=/build \
-	-v $(pwd):/aosp-src \
-	-v $(pwd)/out:/aosp-src/out \
-	-v /tmp/kenvyra_ccache:/tmp/kenvyra_ccache \
-	-e BUILDER_USER="kenvyra" \
-	-e BUILDER_EMAIL="dev@kenvyra.xyz" \
-	-e USE_CCACHE=1 \
-	-e CCACHE_DIR="/tmp/kenvyra_ccache" \
-	-e CCACHE_EXEC="/usr/bin/ccache" \
-	-e HOME="/build" \
-	kenvyra:latest \
-	"$@"
+    --rm \
+    -it \
+    --privileged \
+    --ulimit=host \
+    --ipc=host \
+    --cgroups=disabled \
+    --pids-limit -1 \
+    --name kenvyra \
+    --security-opt label=disable \
+    --userns=keep-id \
+    --hostname $(hostname) \
+    --mount type=tmpfs,tmpfs-size=1G,destination=/build \
+    -v $(pwd):/aosp-src \
+    -v $(pwd)/out:/aosp-src/out \
+    -v /tmp/kenvyra_ccache:/tmp/kenvyra_ccache \
+    -e BUILDER_USER="kenvyra" \
+    -e BUILDER_EMAIL="dev@kenvyra.xyz" \
+    -e USE_CCACHE=1 \
+    -e CCACHE_DIR="/tmp/kenvyra_ccache" \
+    -e CCACHE_EXEC="/usr/bin/ccache" \
+    -e HOME="/build" \
+    kenvyra:latest \
+    "$@"
