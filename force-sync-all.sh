bedb3db65240fbcb6aae4eb6d98f786ba521a41d
Jens Reidel <adrian@travitia.xyz>
2023-03-19 21:36:14 +0100

    scripts: Add more flags to force-sync-all.sh
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- force-sync-all.sh
+++ force-sync-all.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 THREAD_COUNT=$(nproc --all)
-repo sync -c --jobs-network=$(( $THREAD_COUNT < 16 ? $THREAD_COUNT : 16 )) -j$THREAD_COUNT --jobs-checkout=$THREAD_COUNT --force-sync --no-clone-bundle --no-tags
+repo sync -c --jobs-network=$(( $THREAD_COUNT < 16 ? $THREAD_COUNT : 16 )) -j$THREAD_COUNT --jobs-checkout=$THREAD_COUNT --force-sync --no-clone-bundle --no-tags --force-remove-dirty --optimized-fetch --auto-gc
